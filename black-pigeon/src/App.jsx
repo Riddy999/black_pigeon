5923c6262a4c54f75c285b04de4886d1a4df5215
12123434 <e12123434@student.tuwien.ac.at>
2024-02-22 01:27:26 +0100

    implementation: new icon, Map implemented

--- black-pigeon/src/App.jsx
+++ black-pigeon/src/App.jsx
@@ -4,10 +4,8 @@ import 'leaflet/dist/leaflet.css'
 function App() {
 
   return (
-        <MapContainer className='h-full' center={[48.2083, 16.3731]} zoom={13}>
+        <MapContainer zoomControl={false} minZoom={14} maxZoom={18} className='h-full' center={[48.2083, 16.3731]} zoom={14}>
           <TileLayer
-            attribution='© OpenStreetMap Contributors. Tiles courtesy of Humanitarian 
-            OpenStreetMap Team'
             url="http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
           />
         </MapContainer>
